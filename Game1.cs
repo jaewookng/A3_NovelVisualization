e47d3463d0d849527cfcc5c21d0f3e52a6d7b5f2
SrujanU <50220216+SrujanU@users.noreply.github.com>
2026-02-05 16:53:01 -0600

    Add files via upload

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.IO;  
 
 namespace A3_NovelVisualization;
 
@@ -9,16 +12,36 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
+    private SpriteFont _font;
+    private List<string> _allUniqueWords;
+    private List<string> _wordsToDisplay;
+    private Random _random;
+    private KeyboardState _prevKey; 
+    private MouseState _prevMouse;
+    
+    private Color[] _colors = { Color.Gold, Color.White, Color.LightBlue };
+
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
+        _graphics.PreferredBackBufferWidth = 700;
+        _graphics.PreferredBackBufferHeight = 600;
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
     }
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
+        _allUniqueWords = new List<string>();
+        _random = new Random();
+
+        using (var stream = TitleContainer.OpenStream("Content/uniquewords.txt"))
+        using (var reader = new StreamReader(stream))
+        {
+            string fullText = reader.ReadToEnd();
+            string[] splitWords = fullText.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            _allUniqueWords.AddRange(splitWords);
+        }
 
         base.Initialize();
     }
@@ -26,26 +49,65 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+        _font = Content.Load<SpriteFont>("MyFont"); 
+    }
 
-        // TODO: use this.Content to load your game content here
+    private void RefreshWordCloud()
+    {
+        _wordsToDisplay.Clear();
+        for (int i = 0; i < 40 && i < _allUniqueWords.Count; i++)
+        {
+            int index = _random.Next(_allUniqueWords.Count);
+            _wordsToDisplay.Add(_allUniqueWords[index]);
+        }
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+        KeyboardState currentKey = Keyboard.GetState();
+        MouseState currentMouse = Mouse.GetState();
 
-        // TODO: Add your update logic here
+        if (currentKey.IsKeyDown(Keys.Escape)) Exit();
+        
+        if ((currentMouse.LeftButton == ButtonState.Pressed && _prevMouse.LeftButton == ButtonState.Released) ||
+            (currentKey.IsKeyDown(Keys.Enter) && _prevKey.IsKeyUp(Keys.Enter)))
+        {
+            RefreshWordCloud();
+        }
+
+        _prevKey = currentKey; 
+        _prevMouse = currentMouse;
 
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.Black);
 
-        // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        int x = 50;
+        int y = 50;
 
+        foreach (string word in _wordsToDisplay)
+        {
+            Vector2 size = _font.MeasureString(word + "  ");
+            
+            if (x + size.X > 650) 
+            {
+                x = 50;
+                y += (int)size.Y + 10;
+            }
+
+            if (y + size.Y < 580) 
+            {
+                Color displayColor = _colors[_random.Next(_colors.Length)];
+                _spriteBatch.DrawString(_font, word, new Vector2(x, y), displayColor);
+                x += (int)size.X;
+            }
+        }
+
+        _spriteBatch.End();
         base.Draw(gameTime);
     }
 }
